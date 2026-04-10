788711dcf51f3e879f0503b148bd870a357b96ec
cristianfkhym <cristian.ferreira@univale.br>
2026-04-10 18:35:44 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,23 @@
-const CACHE_NAME = 'lisossomo-v6';
+const CACHE_NAME = 'lisossomo-v10';
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(['./', './index.html']);
-    })
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(['./','./index.html']))
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
